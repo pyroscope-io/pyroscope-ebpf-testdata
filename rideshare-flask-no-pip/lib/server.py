4be781722c576cfa17dca8c938b43d89e3ae26eb
Tolya Korniltsev <korniltsev.anatoly@gmail.com>
2024-05-08 18:32:05 +0700

    chore(rideshare-flask-no-pip): make order bike burn more cpu

--- rideshare-flask-no-pip/lib/server.py
+++ rideshare-flask-no-pip/lib/server.py
@@ -5,67 +5,74 @@ from bike.bike import order_bike
 from car.car import order_car
 from scooter.scooter import order_scooter
 
-
 app = Flask(__name__)
+
 
 @app.route("/bike")
 def bike():
-    order_bike(0.2)
-    return "<p>Bike ordered</p>"
+  order_bike(0.4)  # make it equal
+  return "<p>Bike ordered</p>"
 
 
 @app.route("/scooter")
 def scooter():
-    order_scooter(0.3)
-    return "<p>Scooter ordered</p>"
+  order_scooter(0.4)  # make it equal
+  return "<p>Scooter ordered</p>"
 
 
 @app.route("/car")
 def car():
-    order_car(0.4)
-    return "<p>Car ordered</p>"
+  order_car(0.4)  # make it equal
+  return "<p>Car ordered</p>"
+
 
 class CellClsIssueTypeType(type):
-    def __init__(cls, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+  def __init__(cls, *args, **kwargs):
+    super().__init__(*args, **kwargs)
 
-    def __call__(cls, *args, **kwargs):
-        n = 239
-        for i in range(200000):
-            n = n + n
+  def __call__(cls, *args, **kwargs):
+    n = 239
+    for i in range(200000):
+      n = n + n
+
 
 class CellClsIssueType(metaclass=CellClsIssueTypeType):
-    pass
+  pass
+
 
 @app.route("/cell_cls_issue")
 def cell_cls_issue():
-    a = CellClsIssueType()
-    return "<p>cell_cls_issue response</p>"
+  a = CellClsIssueType()
+  return "<p>cell_cls_issue response</p>"
+
 
 class CellSelfIssue:
   def __init__(self):
     self.foo = 239
+
   def bar(self):
     def asd():
       n = 239
       for i in range(200000):
         n = n + n
       return self.foo
+
     return asd()
+
 
 @app.route("/cell_self_issue")
 def cell_self_issue():
-    v = CellSelfIssue().bar()
-    return f"<p>cell_self_issue response{v}</p>"
+  v = CellSelfIssue().bar()
+  return f"<p>cell_self_issue response{v}</p>"
 
 
 @app.route("/")
 def environment():
-    result = "<h1>environment vars:</h1>"
-    for key, value in os.environ.items():
-        result +=f"<p>{key}={value}</p>"
-    return result
+  result = "<h1>environment vars:</h1>"
+  for key, value in os.environ.items():
+    result += f"<p>{key}={value}</p>"
+  return result
+
 
 if __name__ == '__main__':
-    app.run(threaded=False, processes=1, host='0.0.0.0', debug=False)
-
+  app.run(threaded=False, processes=1, host='0.0.0.0', debug=False)
